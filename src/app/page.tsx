afbee6d86915ce390b0cdae0ea7dd4add084b3da
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-10-12 16:14:41 -0400

    keen-slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,35 @@
-import { Button } from '@/components/Button'
-import Header from '@/components/Header'
+"use client"
+
 import Image from 'next/image'
 import Link from 'next/link'
+
+import Header from '@/components/Header'
+import { Button } from '@/components/Button'
+
+import { useKeenSlider } from 'keen-slider/react'
 
 import camiseta1 from '../assets/camisetas/1.png'
 import camiseta2 from '../assets/camisetas/2.png'
 import camiseta3 from '../assets/camisetas/3.png'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  });
+
+
   return (
     <main className='flex flex-col items-start justify-center pl-4'>
-      <Header />
-     <div className='flex gap-12 w-full ml-auto max-w-custom'>
-      <Link href="/product/1" className='bg-gradient-to-b from-green300 to-[#7465d4] rounded-lg p-1 cursor-pointer relative overflow-hidden flex items-center justify-center group'>
+    <Header />
+    
+     <div 
+      className='flex w-full ml-auto max-w-custom keen-slider'
+      ref={sliderRef}
+     >
+      <Link href="/product/1" className='bg-gradient-to-b from-green300 to-[#7465d4] rounded-lg cursor-pointer relative overflow-hidden flex items-center justify-center group keen-slider__slide'>
         <Image src={camiseta1} width={520} height={480} alt="" className="object-cover" />
 
         <footer className='absolute bottom-1 left-1 right-1 p-8 bg-black/60 rounded translate-y-full group-hover:translate-y-0 transition-all duration-200 ease-in-out flex items-center justify-between'>
@@ -21,7 +38,7 @@ export default function Home() {
         </footer>
       </Link>
 
-      <Link href="/product/2" className='bg-gradient-to-b from-green300 to-[#7465d4] rounded-lg p-1 cursor-pointer relative overflow-hidden flex items-center justify-center group'>
+      <Link href="/product/2" className='bg-gradient-to-b from-green300 to-[#7465d4] rounded-lg cursor-pointer relative overflow-hidden flex items-center justify-center group keen-slider__slide'>
         <Image src={camiseta2} width={520} height={480} alt="" className="object-cover" />
 
         <footer className='absolute bottom-1 left-1 right-1 p-8 bg-black/60 rounded translate-y-full group-hover:translate-y-0 transition-all duration-200 ease-in-out flex items-center justify-between'>
@@ -29,11 +46,20 @@ export default function Home() {
           <span className='text-2xl font-semibold text-green-300'>R$ 79,90</span>
         </footer>
       </Link>
-      <Link href="/product/3" className='bg-gradient-to-b from-green300 to-[#7465d4] rounded-lg p-1 cursor-pointer relative overflow-hidden flex items-center justify-center group'>
+      <Link href="/product/3" className='bg-gradient-to-b from-green300 to-[#7465d4] rounded-lg cursor-pointer relative overflow-hidden flex items-center justify-center group keen-slider__slide'>
         <Image src={camiseta3} width={520} height={480} alt="" className="object-cover" />
 
         <footer className='absolute bottom-1 left-1 right-1 p-8 bg-black/60 rounded translate-y-full group-hover:translate-y-0 transition-all duration-200 ease-in-out flex items-center justify-between'>
           <strong className='text-xl'>Camiseta X3</strong>
+          <span className='text-2xl font-semibold text-green-300'>R$ 79,90</span>
+        </footer>
+      </Link>
+
+        <Link href="/product/2" className='bg-gradient-to-b from-green300 to-[#7465d4] rounded-lg cursor-pointer relative overflow-hidden flex items-center justify-center group keen-slider__slide'>
+        <Image src={camiseta2} width={520} height={480} alt="" className="object-cover" />
+
+        <footer className='absolute bottom-1 left-1 right-1 p-8 bg-black/60 rounded translate-y-full group-hover:translate-y-0 transition-all duration-200 ease-in-out flex items-center justify-between'>
+          <strong className='text-xl'>Camiseta X2</strong>
           <span className='text-2xl font-semibold text-green-300'>R$ 79,90</span>
         </footer>
       </Link>
