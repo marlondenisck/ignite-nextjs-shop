0ac9e6af43c6fc8d348aefbfd6b5752bcbac6836
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-10-15 20:36:43 -0400

    teste com Server Components

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
 // import { Container } from './styles';
+async function getData() {
+  await new Promise(resolve => setTimeout(resolve, 2000))
+  console.log('rodou')
+  return [1, 2, 3]
+}
 
-function Success() {
-  return <h1>Success Page</h1>;
+
+async function Success() {
+   const list = await getData()
+  return (
+    <div>
+      <h1>Success Page</h1>
+      <ul>
+        {list.map(item => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Success;
